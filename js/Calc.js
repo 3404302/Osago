20dcb3ce88d361afa553d8888d7441c0403627b2
3404302 <3404302@gmail.com>
2018-03-09 11:51:47 +0300

    tdfghv

--- js/Calc.js
+++ js/Calc.js
@@ -15,15 +15,30 @@ function Calc() {
 /**
  * Формула расчета страховой премии ФИЗИКИ
  */
-
-			var a = 4118 * tb * kt * kbm * kvs * ko * km * kp;
-            var b = 3432 * tb * kt * kbm * kvs * ko * km * kp;
+    
+    var a = 3432 * tb * kt * kbm * kvs * ko * km * kp;    // alert([a.toFixed(2),a.toFixed(2)] ) Огругляем до двух знаков после запятой;
+    var b = 3700 * tb * kt * kbm * kvs * ko * km * kp;
+    var c = 4118 * tb * kt * kbm * kvs * ko * km * kp;
     
 /**
  * Формула расчета страховой премии ЮРИКИ
  */
+    
+    var y = 2573 * tb * kt * kbm * ko * km * kp * kpr;
+    
+/**
+ * Результаты
+ */
  
-			document.getElementById('result').innerHTML = a + " * " + b + " = " + a;
-            document.getElementById('result1').innerHTML = a + " + " + b + " = " + b;
+    document.getElementById('result').innerHTML = "3432" + " * " + kt + " * " + kbm + " * " + kvs + " * " + km + " * " + kp;
+    document.getElementById('result-1').innerHTML = + a.toFixed(2);
+
+    document.getElementById('result2').innerHTML = "3700" + " * " + kt + " * " + kbm + " * " + kvs + " * " + km + " * " + kp;
+    document.getElementById('result-2').innerHTML = + b.toFixed(2);
+    
+    document.getElementById('result3').innerHTML = "4118" + " * " + kt + " * " + kbm + " * " + kvs + " * " + km + " * " + kp;
+    document.getElementById('result-3').innerHTML = + c.toFixed(2);
+    
+    //document.getElementById('result1').innerHTML = a + " + " + b + " = " + b;
             
 		}
