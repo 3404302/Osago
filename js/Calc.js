d620f499c5157a5af48f4fe0231021c96f9ac712
3404302 <3404302@gmail.com>
2018-03-09 10:15:00 +0300

    ыфв

--- js/Calc.js
+++ js/Calc.js
@@ -1,11 +1,29 @@
 function Calc() {
-			var a = +document.getElementById('Kt').value;
-			var b = +document.getElementById('Kbc').value;
 
-			var c = 4118 * a * b;
-            var d = 3432 * a * b;
+/**
+ * Соотносим коэффициенты в соответствии с выбором
+ */
+    var tb = +document.getElementById('tb').value; /* Базовая ставка 3432-4118 . зависит от территории, категории авто и страховой компании */
+    var kt = +document.getElementById('kt').value; /* коэф территории */
+    var kbm = +document.getElementById('kbm').value; /* коэф бонус-малус */
+    var kvs = +document.getElementById('kvs').value; /* коэф возраст-стаж */
+    var ko = +document.getElementById('ko').value; /* без ограничений - если выбран - коэф 1.8 (пока один) */
+    var km = +document.getElementById('km').value; /* коэф мощность */
+    var kp = +document.getElementById('kp').value; /* сезонность */
+    var kpr = +document.getElementById('kpr').value; /* прицеп (только юрики) */
+
+/**
+ * Формула расчета страховой премии ФИЗИКИ
+ */
+
+			var a = 4118 * tb * kt * kbm * kvs * ko * km * kp;
+            var b = 3432 * tb * kt * kbm * kvs * ko * km * kp;
+    
+/**
+ * Формула расчета страховой премии ЮРИКИ
+ */
  
-			document.getElementById('result').innerHTML = a + " * " + b + " = " + c;
-            document.getElementById('result1').innerHTML = a + " + " + b + " = " + d;
+			document.getElementById('result').innerHTML = a + " * " + b + " = " + a;
+            document.getElementById('result1').innerHTML = a + " + " + b + " = " + b;
             
 		}
