f36acdfc9aa39be3c16c2550cede98cb4bc4bdee
3404302 <3404302@gmail.com>
2018-03-27 01:20:19 +0300

    asd

--- js/Calc.js
+++ js/Calc.js
@@ -10,8 +10,10 @@ function Calc() {
     var kvs = +document.getElementById('kvs').value; // коэф возраст-стаж
     var ko = +document.getElementById('ko').value; // без ограничений - если выбран - коэф 1.8 (пока один)
     var km = +document.getElementById('km').value; // коэф мощность
-    var kp = +document.getElementById('kp').value; // сезонность
+    var ks = 1; // срок страхования
+    var kp = +document.getElementById('kp').value; // период использования
     var kpr = +document.getElementById('kpr').value; // прицеп (только юрики)
+    var kn = 1; // коэф грубых нарушений
     
     if (+document.getElementById('ko').checked || type > 1) {
             kvs = 1;
@@ -24,16 +26,16 @@ function Calc() {
  */
     var a = 'Формула расчета премии'
     if (type == 1) {
-        a = 3432 * tb * kt * kbm * kvs * ko * km * kp; //Физик, ko нет, берем kvs по if выше
+        a = 3432 * kt * kbm * kvs * ko * ks * kp * km * kn; //Физик, ko нет, берем kvs по if выше
     } else {
-        a = 2573 * tb * kt * kbm * kvs * ko * km * kp * kpr; //Юрик, kvs нет, берем ko = 1.8
+        a = 2573 * kt * kbm * kvs * ko * ks * kp * km * kpr * kn; //Юрик, kvs нет, берем ko = 1.8
     }
     
     var aa = 'Результат'
     if (type == 1) {
-        aa = "3432" + " * " + kt + " * " + kbm + " * " + kvs + " * " + ko + " * " + km + " * " + kp + " * " + "1"; //Физик
+        aa = "3432" + " * " + kt + " * " + kbm + " * " + kvs + " * " + ko + " * " + ks + " * " + kp + " * " + km + " * " + "1" + " * " + kn; //Физик
     } else {
-        aa = "2573" + " * " + kt + " * " + kbm + " * " + kvs + " * " + ko + " * " + km + " * " + kp + " * " + kpr; //Юрик
+        aa = "2573" + " * " + kt + " * " + kbm + " * " + kvs + " * " + ko + " * " + ks + " * " + kp + " * " + km + " * " + kpr + " * " + kn; //Юрик
     }
     
         //var a = 3432 * tb * kt * kbm * kvs * ko * km * kp;    // alert([a.toFixed(2),a.toFixed(2)] ) Огругляем до двух знаков после запятой
@@ -45,10 +47,10 @@ function Calc() {
  */
     
     var nasko = "Кв наско";
-    if (km <= 1.1 || kp < 1 ) {
+    if (km <= 1.1 || kp < 1 || tb != 2) {
         nasko = "запрет";
     } else { 
-        nasko = "23%, Кроме: ВАЗ/LADA/ГАЗ/УАЗ, RENAULT LOGAN, DAEWOO NEXIA СТАРШЕ 3-х лет от года выпуска по ПТС";
+        nasko = "23%, Кроме: Ваз/Lada/Газ/Уаз, </br>Renault Logan, Daewoo Nexia </br>СТАРШЕ 3-х лет от г.в. по ПТС";
     }
     
     
