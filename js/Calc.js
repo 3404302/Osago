ef45766d71f61bdf3cdb58f713d16d37467ceab3
3404302 <3404302@gmail.com>
2018-03-27 00:51:22 +0300

    saffsd

--- js/Calc.js
+++ js/Calc.js
@@ -3,35 +3,68 @@ function Calc() {
 /**
  * Соотносим коэффициенты в соответствии с выбором
  */
-    var tb = +document.getElementById('tb').value; /* Базовая ставка 3432-4118 . зависит от территории, категории авто и страховой компании */
-    var kt = +document.getElementById('kt').value; /* коэф территории */
-    var kbm = +document.getElementById('kbm').value; /* коэф бонус-малус */
-    var kvs = +document.getElementById('kvs').value; /* коэф возраст-стаж */
-    var ko = +document.getElementById('ko').value; /* без ограничений - если выбран - коэф 1.8 (пока один) */
-    var km = +document.getElementById('km').value; /* коэф мощность */
-    var kp = +document.getElementById('kp').value; /* сезонность */
-    var kpr = +document.getElementById('kpr').value; /* прицеп (только юрики) */
+    var type = +document.getElementById('type').value; // Физик или юрик
+    var tb = +document.getElementById('tb').value; // Базовая ставка 3432-4118 . зависит от территории, категории авто и страховой компании
+    var kt = +document.getElementById('kt').value; // коэф территории
+    var kbm = +document.getElementById('kbm').value; // коэф бонус-малус
+    var kvs = +document.getElementById('kvs').value; // коэф возраст-стаж
+    var ko = +document.getElementById('ko').value; // без ограничений - если выбран - коэф 1.8 (пока один)
+    var km = +document.getElementById('km').value; // коэф мощность
+    var kp = +document.getElementById('kp').value; // сезонность
+    var kpr = +document.getElementById('kpr').value; // прицеп (только юрики)
+    
+    if (+document.getElementById('ko').checked || type > 1) {
+            kvs = 1;
+        } else {
+            ko = 1;
+        }
 
 /**
  * Формула расчета страховой премии ФИЗИКИ
  */
+    var a = 'Формула расчета премии'
+    if (type == 1) {
+        a = 3432 * tb * kt * kbm * kvs * ko * km * kp; //Физик, ko нет, берем kvs по if выше
+    } else {
+        a = 2573 * tb * kt * kbm * kvs * ko * km * kp * kpr; //Юрик, kvs нет, берем ko = 1.8
+    }
     
-    var a = 3432 * tb * kt * kbm * kvs * ko * km * kp;    // alert([a.toFixed(2),a.toFixed(2)] ) Огругляем до двух знаков после запятой;
+    var aa = 'Результат'
+    if (type == 1) {
+        aa = "3432" + " * " + kt + " * " + kbm + " * " + kvs + " * " + ko + " * " + km + " * " + kp + " * " + "1"; //Физик
+    } else {
+        aa = "2573" + " * " + kt + " * " + kbm + " * " + kvs + " * " + ko + " * " + km + " * " + kp + " * " + kpr; //Юрик
+    }
+    
+        //var a = 3432 * tb * kt * kbm * kvs * ko * km * kp;    // alert([a.toFixed(2),a.toFixed(2)] ) Огругляем до двух знаков после запятой
     var b = 3700 * tb * kt * kbm * kvs * ko * km * kp;
     var c = 4118 * tb * kt * kbm * kvs * ko * km * kp;
+    
+/**
+ * Проверка расчета
+ */
+    
+    var nasko = "Кв наско";
+    if (km <= 1.1 || kp < 1 ) {
+        nasko = "запрет";
+    } else { 
+        nasko = "23%, Кроме: ВАЗ/LADA/ГАЗ/УАЗ, RENAULT LOGAN, DAEWOO NEXIA СТАРШЕ 3-х лет от года выпуска по ПТС";
+    }
+    
     
 /**
  * Формула расчета страховой премии ЮРИКИ
  */
     
-    var y = 2573 * tb * kt * kbm * ko * km * kp * kpr;
+    //var y = 2573 * tb * kt * kbm * ko * km * kp * kpr;
     
 /**
  * Результаты
  */
  
-    document.getElementById('result').innerHTML = "3432" + " * " + kt + " * " + kbm + " * " + kvs + " * " + km + " * " + kp;
-    document.getElementById('result-1').innerHTML = + a.toFixed(2);
+    document.getElementById('result').innerHTML = + a.toFixed(2);
+    document.getElementById('result-1').innerHTML = aa;
+    document.getElementById('result-1-1').innerHTML = nasko;
 
     document.getElementById('result2').innerHTML = "3700" + " * " + kt + " * " + kbm + " * " + kvs + " * " + km + " * " + kp;
     document.getElementById('result-2').innerHTML = + b.toFixed(2);
